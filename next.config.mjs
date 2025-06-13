86d65b6abf78b109b76e2ce5f3df75754b4626cf
sayagaffy <rgaffyagb@gmail.com>
2025-06-13 09:20:31 +0700

    update 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   env: {
+    NEXT_PUBLIC_SITE_URL: "https://viannusamandiri.id",
     SMTP_HOST: process.env.SMTP_HOST,
     SMTP_PORT: process.env.SMTP_PORT,
     SMTP_USER: process.env.SMTP_USER,
     SMTP_PASS: process.env.SMTP_PASS,
     SMTP_FROM: process.env.SMTP_FROM,
     CONTACT_EMAIL: process.env.CONTACT_EMAIL,
+  },
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
   },
 };
 
